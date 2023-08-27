4819aef45f161580cfb1f895f379f9170dddab30
Tim Eitel <timeitel@outlook.com>
2023-08-27 16:44:36 +1000

    feat: scene plugin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod modules;
 
 use bevy::prelude::*;
-use modules::player::PlayerPlugin;
+use modules::{player::PlayerPlugin, scene::ScenePlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(PlayerPlugin)
+        .add_plugins((PlayerPlugin, ScenePlugin))
         .run();
 }
